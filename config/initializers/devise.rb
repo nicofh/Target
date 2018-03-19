1c650f7b20f3b46eb5eed50f7b4de84eeba8095c
Nicolás <nicofh93@gmail.com>
2018-03-19 15:27:40 -0300

    coment delete

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,4 @@
-# https://target-nico-staging.herokuapp.com/https://target-nico-staging.herokuapp.com/frozen_string_literal: true
+# frozen_string_literal: true
 
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
